e36ba750b6a493ff10e1ee8667a5bb20c3d12258
Flowingblood <da.ka@freenet.de>
2021-11-01 21:16:15 +0100

    A

--- Test9.rkt
+++ Test9.rkt
@@ -1,4 +1,6 @@
-#lang racket
+#lang racket/base
+
+(require rackunit)
 
 (define (my-length lst)
   
@@ -11,3 +13,7 @@
   ;call the foldl function which runs over each element from the list
   (foldl my-length-inner 0 lst)
 )
+
+(check-equal? (my-length (list 1 2 3)) 3)
+
+(check-equal? (my-length (list 1 2 3 4 5 6 7 8 9)) 9)
