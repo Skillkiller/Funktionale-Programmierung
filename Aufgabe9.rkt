680b80fed421357dd0ccbba3cbe956b1561992d6
Flowingblood <da.ka@freenet.de>
2021-11-01 21:13:59 +0100

    A

--- Aufgabe9.rkt
+++ Aufgabe9.rkt
@@ -1,12 +1,11 @@
 #lang racket
 
-(define lst (list 3 4 5 6 7 8))
-
 (define (my-length lst)
+  
+  ;Define the inner function which has the two parameters for foldl/r
   (define (my-length-inner p0 counter)
+    ;Ignore the p0 parameter and increment the counter
     (+ counter 1))
-
+  ;call the foldl function which runs over each element from the list
   (foldl my-length-inner 0 lst)
 )
-
-(display (my-length lst))
